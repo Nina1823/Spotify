aed790fab9c02d41d7765b76701e792e6294474d
Nina1823 <lilianarivera310@gmail.com>
2024-04-18 12:45:11 -0500

    Feat: Modules and tracks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+//   status: string | number =22;
   title = 'spotify';
+//   name: string = 'Lili';
+//   age: number = 28
+//   phone: null = null;
+//   datum:any;
+
+//   //obj
+//   // car:any ={
+//   //   brand:'ford',
+//   //   model:'Focus',
+//   //   year:2021
+//   // }
+//  carModel:any ={
+//     brand:'ford',
+//     model:'Focus',
+//     year:2021
+//   }
+
+//   listCars:Array<CarModel> = [
+//     {
+//     brand:'chevrolet',
+//     model:'camaro',
+//     year:2021
+//   },
+//   {
+//     brand:'BMW',
+//     model:'AMG 200',
+//     year:2024
+//   }]
+// }
+//  //interfaz
+//  interface CarModel{
+//   brand: string;
+//   model:string;
+//   year?:number; //? que puede ser opcional
 }
